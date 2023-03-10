3487eb8dbda0adad1f6311decb346e034fd95176
Arul_Murugan_R <arulmuruganarjun14@gmail.com>
2023-03-10 19:02:58 +0530

    module css

--- Frontend/src/components/Cards/Card.jsx
+++ Frontend/src/components/Cards/Card.jsx
@@ -1,43 +1,48 @@
-import './Cards.css'
+import classes from './Cards.module.css'
 import { Link } from 'react-router-dom'
+import { useRef , useEffect } from 'react'
 const Cards = (props) =>{
+    const myRef = useRef();
+    useEffect(() => {
+        console.log(myRef.current.offsetWidth)
+    },[])
     const { title, user, req , img , type,hospital,location,date,hosAddr,units } = props.details
     return (
         <>
-        <div className="user-item">
-                        <div className="user-item-card">
-                            <div className="content">
+        <div className={classes.userItem} ref={myRef}>
+                        <div className={classes.userItemCard}>
+                            <div className={classes.content}>
                                 <label>{title?title:'Request For Blood'}</label>
                                 {user&&<span
                                 >Email:
-                                    <span className="content-title"
+                                    <span className={classes.contentTitle}
                                     >{user}</span>
                                 </span>}
                                 {hospital&&<span
                                 >Hospital:
-                                    <span className="content-title"
+                                    <span className={classes.contentTitle}
                                     >{" ".concat(...hospital.split(" ",2))}</span>
                                 </span>}
                                 {date&&<span
                                 >Deadline:
-                                    <span className="content-title"
+                                    <span className={classes.contentTitle}
                                     >{" "+date.split('T')[0]}</span>
                                 </span>}
                                 {units&&<span
                                 >Units:
-                                    <span className="content-title"
-                                    >{' '+units+(' '+props.i)}</span>
+                                    <span className={classes.contentTitle}
+                                    >{' '+units}</span>
                                 </span>}
                                 {/* {location&&<span
                                 >Location:
-                                    <span className="content-title"
+                                    <span className={classes.contentTitle}
                                     >{location}</span>
                                 </span>} */}
 
                                 <span><small>{props.children}</small></span>
                             </div>
-                            <div className="user-item-media">
-                                <div className="media-top">
+                            <div className={classes.userItemMedia}>
+                                <div className={classes.mediaTop}>
                                     {img&&<img
                                         src="https://tinypic.host/images/2023/02/21/pikrepo.com-2.jpg"
                                         alt="Image"
@@ -46,11 +51,11 @@ const Cards = (props) =>{
                                 </div>
                             </div>
                         </div>
-                        {type=='request'&&<div className="user-item-request">
+                        {type=='request'&&<div className={classes.userItemRequest}>
                                 {/* <Link to="/more" className="link">
                                     Accept
                                 </Link> */}
-                                <Link to="/more" className="link">
+                                <Link onClick={()=>{props.openHandler(props.details)}} className={classes.link}>
                                     View Details
                                 </Link>
                         </div>}
--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -1,67 +1,67 @@
-import "./Footer.css";
+import classes from "./Footer.module.css";
 
 const Footer = () => {
 	return (
 		<>
-			<footer className="padding_4x">
-				<div className="flex">
-					<section className="flex-content padding_1x">
+			<footer className={classes["padding_4x"]}>
+				<div className={classes.flex}>
+					<section className={classes["flex-content"] + " " + classes["padding_1x"]}>
 						<h3>Top Products</h3>
 						<a href="#">Managed Website</a>
 						<a href="#">Manage Reputation</a>
 						<a href="#">Power Tools</a>
 						<a href="#">Marketing Service</a>
 					</section>
-					<section className="flex-content padding_1x">
+					<section className={classes["flex-content"] + " " + classes["padding_1x"]}>
 						<h3>Quick Links</h3>
 						<a href="#">Jobs</a>
 						<a href="#">Brand Assets</a>
 						<a href="#">Investor Relations</a>
 						<a href="#">Terms of Service</a>
 					</section>
-					<section className="flex-content padding_1x">
+					<section className={classes["flex-content"] + " " + classes["padding_1x"]}>
 						<h3>Features</h3>
 						<a href="#">Jobs</a>
 						<a href="#">Brand Assets</a>
 						<a href="#">Investor Relations</a>
 						<a href="#">Terms of Service</a>
 					</section>
-					<section className="flex-content padding_1x">
+					<section className={classes["flex-content"] + " " + classes["padding_1x"]}>
 						<h3>Resources</h3>
 						<a href="#">Guides</a>
 						<a href="#">Research</a>
 						<a href="#">Experts</a>
 						<a href="#">Agencies</a>
 					</section>
-					<section className="flex-content padding_1x">
+					<section className={classes["flex-content"] + " " + classes["padding_1x"]}>
 						<h3>Newsletter</h3>
 						<p>You can trust us. we only send promo offers,</p>
-						<fieldset className="fixed_flex">
+						<fieldset className={classes["fixed_flex"]}>
 							<input
 								type="email"
 								name="newsletter"
 								placeholder="Your Email Address"
 							/>
-							<button className="btn btn_2">Subscribe</button>
+							<button className={classes["btn"] + " " + classes["btn_2"]}>Subscribe</button>
 						</fieldset>
 					</section>
 				</div>
-				<div className="flex">
-					<section className="flex-content padding_1x">
+				<div className={classes.flex}>
+					<section className={classes["flex-content"] + " " + classes["padding_1x"]}>
 						<p>Copyright ©2023 All rights reserved || Blood Bank</p>
 					</section>
-					<section className="flex-content padding_1x">
+					<section className={classes["flex-content"]  + " " + classes["padding_1x"]}>
 						<a href="#">
-							<i className="fa fa-facebook"></i>
+							<i className={classes.fa + " " + classes["fa-facebook"]}></i>
 						</a>
 						<a href="#">
-							<i className="fa fa-twitter"></i>
+							<i className={classes.fa + " " + classes["fa-twitter"]}></i>
 						</a>
 						<a href="#">
-							<i className="fa fa-dribbble"></i>
+							<i className={classes.fa + " " + classes["fa-dribbble"]}></i>
 						</a>
 						<a href="#">
-							<i className="fa fa-linkedin"></i>
+							<i className={classes.fa + " " + classes["fa-linkedin"]}></i>
 						</a>
 					</section>
 				</div>
