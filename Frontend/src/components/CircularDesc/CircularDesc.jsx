33bb9a66d000a19c10f999855f7c427adb107104
Arul_Murugan_R <arulmuruganarjun14@gmail.com>
2023-03-27 22:54:40 +0530

    small fixs

--- Frontend/src/components/CircularDesc/CircularDesc.jsx
+++ Frontend/src/components/CircularDesc/CircularDesc.jsx
@@ -6,8 +6,8 @@ import { color } from '@mui/system'
 const CircularDesc = () => {
     const [desc, setDesc] = useState(
         {
-            label: 'A+',
-            count: '5000',
+            label: 'AB+',
+            count: '2500',
             color: '#00bfa5'
         }
     )
@@ -20,7 +20,7 @@ const CircularDesc = () => {
 							count: i,
 						};
 				});
-			}, 30);
+			}, 50);
 		}
 	};
     const layerHandler = (e) => {
@@ -32,33 +32,20 @@ const CircularDesc = () => {
             }
         })
     }
+    var deg = 0
     return (
         <div className="wrapper-desc">
             <div className="circ-cont">
-                <div className='bloods' style={{ "--angle": " 0deg" }} onClick={layerHandler}>
-                    <label>A+</label>
-                </div>
-                <div className='bloods' style={{ "--angle": " 45deg" }} onClick={layerHandler}>
-                    <label>B+</label>
-                </div>
-                <div className='bloods' style={{ "--angle": " 90deg" }} onClick={layerHandler}>
-                    <label>AB+</label>
-                </div>
-                <div className='bloods' style={{ "--angle": " 135deg" }} onClick={layerHandler}>
-                    <label>O+</label>
-                </div>
-                <div className='bloods' style={{ "--angle": " 180deg" }} onClick={layerHandler}>
-                    <label>B-</label>
-                </div>
-                <div className='bloods' style={{ "--angle": " 225deg" }} onClick={layerHandler}>
-                    <label>A-</label>
-                </div>
-                <div className='bloods' style={{ "--angle": " 270deg" }} onClick={layerHandler}>
-                    <label>O-</label>
-                </div>
-                <div className='bloods' style={{ "--angle": " 315deg" }} onClick={layerHandler}>
-                    <label>M</label>
-                </div>
+                {descData.map((item,index) => {
+                    return(<div className='bloods' key={item.label} style={{ "--angle":index*45+"deg",
+                    background:`${item.label==desc.label?'black':''}`,
+                    border:`${item.label==desc.label?'1px solid red':''}`,
+                    color:`${item.label==desc.label?'#ccc':''}`
+                    }} onClick={layerHandler}>
+                        <label>{item.label}</label>
+                    </div>)
+            }
+           )}
                 <div className="inner-circle" style={{"background":"rgb(181, 30, 35)"}}>
                     <label>{desc.label}ve Donor<br />
                         <b>{desc.count}+</b></label>
